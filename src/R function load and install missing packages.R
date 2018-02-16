2d170e16be94d1f35dc3958619bdf242298d38f6
Mike <8523449+wolfm4ne@users.noreply.github.com>
2018-02-16 08:29:08 -0500

    Made changes here and then moved.

--- src/R function load and install missing packages.R
+++ src/R function load and install missing packages.R
@@ -1,16 +1,21 @@
-# function to install packages if missing by user
+#' Package installing and loading function
+#' 
+#' The usePackage function takes a single command, the name of a package, and will install it (if not installed) and then load it.
+#' @param p A package name as a string in quotes.
+#' @keywords use package install load library
+#' @export
+#' @examples 
+#' usePackage("readr")
+
 usePackage<-function(p){
-  # load a package if installed, else load after installation.
-  # Args:
-  #   p: package name in quotes
   
-  if (!is.element(p, installed.packages()[,1])){
-    message(paste("Package:",p,"not found, installing now..."))
-    install.packages(p, dep = TRUE)}
-  message(paste("Loading Package:",p))
-  require(p, character.only = TRUE)  
+  if (!is.element(p, installed.packages()[, 1])) {
+    
+    message(paste("Package", p,"not found, installing..."))
+    install.packages(p, dep = TRUE)
+    
+    }
+  message(paste0("Loading Package ", p, "..."))
+  require(p, character.only = TRUE)
+  
 }
-
-
-# Example
-usePackage("readr")  # import/export csv
